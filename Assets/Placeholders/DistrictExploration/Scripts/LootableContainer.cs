5a2acb7d150eddcdc08552a40c82000fcc9ca395
landonscribbles <landonscribbles@gmail.com>
2017-03-22 16:24:51 -0700

    Work on displayable items

---
+++ Assets/Placeholders/DistrictExploration/Scripts/LootableContainer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootableContainer : MonoBehaviour {
+
+    public class LootableItems {
+
+    	private int metals = 0;
+    	public int Metals {
+            get {
+                return metals;
+            }
+            set {
+                metals = value;
+            }
+    	}
+
+        private int fabrics = 0;
+        public int Fabrics {
+            get {
+                return fabrics;
+            }
+            set {
+                fabrics = value;
+            }
+        }
+
+        private int fuel = 0;
+        public int Fuel {
+            get {
+                return fuel;
+            }
+            set {
+                fuel = value;
+            }
+        }
+
+        private int electronics = 0;
+        public int Electronics {
+            get {
+                return electronics;
+            }
+            set {
+                electronics = value;
+            }
+        }
+
+        private int provisions = 0;
+        public int Provisions {
+            get {
+                return provisions;
+            }
+            set {
+                provisions = value;
+            }
+        }
+
+        private List<DisplayableItemInterface> displayableItems;
+    }
+
+}
---
+++ Assets/Placeholders/Scripts/Inventory/DisplayableItemInterface.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface DisplayableItemInterface {
+
+    Image GetDisplayImage();
+
+    string GetItemDescripion();
+
+
+}
